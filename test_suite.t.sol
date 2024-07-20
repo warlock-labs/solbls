2b071be2845436e58db5834c7009305da38819af
individualWhoCodes <mbm2212@columbia.edu>
2024-07-20 10:52:54 -0500

    Update test_suite.t.sol

--- test_suite.t.sol
+++ test_suite.t.sol
@@ -9,14 +9,13 @@ import "../src/test_suite.sol";
 // This is a Foundry contract written in Solidity, to test
 // the BLS.sol library, via the the test contract of test_suite.sol
 
-// test cases are pulled from bn254_reference.json, generated using SAGE MATH
-// a = parseJSON("bn254_reference.json");
+// Many of the test cases are pulled from bn254_reference_transformed.json, generated using SAGE MATH
 
-// a['G1_signatures'] list of format {'x': uint256, 'y': uint256}
-// a['G2_public_keys'] list of format {'x': {'c0': uint256, 'c1': uint256}, 'y': {'c0': uint256, 'c1': uint256}}
-// a['E2_non_G2'] list of format {'x': {'c0': uint256, 'c1': uint256}, 'y': {'c0': uint256, 'c1': uint256}}
-// a['private_keys'] list of format uint256
-// a['svdw'] list of format {'i': uint256, 'x': uint256, 'y': uint256}
+// json['G1_signatures'] list of format {'x': uint256, 'y': uint256}
+// json['G2_public_keys'] list of format {'x': {'c0': uint256, 'c1': uint256}, 'y': {'c0': uint256, 'c1': uint256}}
+// json['E2_non_G2'] list of format {'x': {'c0': uint256, 'c1': uint256}, 'y': {'c0': uint256, 'c1': uint256}}
+// json['private_keys'] list of format uint256
+// json['svdw'] list of format {'i': uint256, 'x': uint256, 'y': uint256}
 
 // A reference Hardhat implementation of is found at BLS.ts
 // BLS.ts relies upon a few functions from utils.ts
@@ -25,67 +24,27 @@ import "../src/test_suite.sol";
 // by default. Fortunately, Foundry exposes a parseJSON() set of
 // functions, to effectively read and feed these into the contract.
 
-/*
-struct G1SigPair
-{
-    uint256 x;
-    uint256 y;
-}
-
-struct G2PubKeys
-{
-    uint256 x_c0;
-    uint256 x_c1;
-    uint256 y_c0;
-    uint256 y_c1;
-}
-
-struct E2_non_G2
-{
-    uint256 x_c0;
-    uint256 x_c1;
-    uint256 y_c0;
-    uint256 y_c1;
-}
-
-struct SVDW
-{
-    uint256 i;
-    uint256 x;
-    uint256 y;
-}
-struct JsonInputs
-{
-    uint256[][] e2_non_g2;
-    uint256[][] g1_signatures;
-    uint256[][] g2_public_keys;
-    uint256[][] private_keys;
-    uint256[][] svdw;
-}
-*/
-
 contract test_suiteTest is Test
 {
+    // EC Test Cases
     uint256[][] e2_non_g2;
     uint256[][] g1_signatures;
     uint256[][] g2_public_keys;
     uint256[][] svdw;
-    //bytes[] randByteTests;
+
+    // Hashing and Padding Variables
     uint256 zero = 0;
-    uint64 zeroSixFour = 0;
     string domain = "BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_";
-    bytes zpad = abi.encodePacked(abi.encodePacked(zero, zero, zero, zero), zeroSixFour);
+    bytes zpad = abi.encodePacked(abi.encodePacked(zero, zero, zero, zero), uint64(0));
     bytes field_order = hex"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47";
     BLSTest bls = new BLSTest();
+
+    // Test Case Read
     string json = vm.readFile("/Users/mitch/Desktop/solBlsTest/blsTest/test/bn254_reference_transformed.json");
     uint256[] private_keys = vm.parseJsonUintArray(json, ".private_keys");
-    // string jsonTwo = vm.readFile("/Users/mitch/Desktop/solBlsTest/blsTest/test/json3.json");
     function setUp() noGasMetering public
     {
-        /*string memory left = string.concat(".", Strings.toString(10));
-        string memory lookup = string.concat(left, "");
-        randByteTests = vm.parseJsonBytesArray(jsonTwo, lookup);*/
-        for(uint256 i = 0; i < 1000; i++)
+       for(uint256 i = 0; i < 1000; i++)
         {
             string memory strNum = Strings.toString(i);
             string memory left = string.concat("[", strNum);
@@ -100,71 +59,47 @@ contract test_suiteTest is Test
             svdw.push(rawSVDW);
         }
     }
-    //bytes rawG1 = vm.parseJson(json, ".G1_signatures[0]");
-    //G1SigPair g1_signatures = abi.decode(rawG1, (G1SigPair));
-    //JsonInputs jsonInput = abi.decode(data, (JsonInputs));
-    /*function testPrint() public view
-    {
-        console.logBytes(randByteTests[0]);
-        console.logBytes(randByteTests[1]);
-        console.logBytes(abi.encodePacked(zpad));
-        // Ok, these byte reads work
-        // Its time for us to push a bunch of them through
-        // encode_message and hash_to_field until they work
-
-        /*console.log(private_keys[54]);
-        console.log(g1_signatures[54][0],g1_signatures[54][1]);
-        console.log(g2_public_keys[54][0],g2_public_keys[54][1],g2_public_keys[54][2],g2_public_keys[54][3]);
-        console.log(e2_non_g2[54][0],e2_non_g2[54][1],e2_non_g2[54][2],e2_non_g2[54][3]);
-        console.log(svdw[54][0],svdw[54][1],svdw[54][2]);
-        //console.log(jsonInput.svdw[0]);
-    }*/
-
     function testExpand_Message(bytes memory expMsg) public view
     {
+        // Hashing string to element of the field should work
         uint8 outputLen = 96;
         uint8 domainLength = 43;
-        //console2.logBytes(msgData);
-        console2.logBytes(expMsg);
-        //string memory domain = "BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_";
+
         bytes memory b_0 = abi.encodePacked(zpad,expMsg,uint8(outputLen >> 8),uint8(outputLen & 255), uint8(0),bytes(domain),domainLength);
-        console2.logBytes(b_0);
         bytes32 b_0_hashed = keccak256(b_0);
-        console2.logBytes32(b_0_hashed);
+
+        // in BLS.ts this step is done in a for loop
         bytes memory b_i = abi.encodePacked(b_0_hashed,uint8(1),bytes(domain),domainLength);
         bytes32 b_i_hashed = keccak256(b_i);
-        console2.logBytes32(b_i_hashed);
         bytes memory newb_i = abi.encodePacked(b_i_hashed ^ b_0_hashed, uint8(2), bytes(domain),domainLength);
         bytes32 newHash =keccak256(newb_i);
         bytes memory newb_ii = abi.encodePacked(newHash ^ b_0_hashed, uint8(3), bytes(domain),domainLength);
         bytes32 secondHash = keccak256(newb_ii);
-        console.logBytes(abi.encodePacked(b_i_hashed,newHash,secondHash));
+
+        // assert equivalence between our implementation and the library
         assert(keccak256(bls.expandMsgTo96(bytes(domain), expMsg)) == keccak256(abi.encodePacked(b_i_hashed,newHash,secondHash)));
-        // copy first 32 bytes of b_i_hashed into a 96 byte array three times, then return array
-        // return out
     }
     function expand_message(bytes memory message) private view returns (bytes memory)
     {
+        // same as above, but used by testHash_to_field, privately
         uint8 outputLen = 96;
         uint8 domainLength = 43;
-        //console2.logBytes(msgData);
-        console2.logBytes(message);
+
         bytes memory b_0 = abi.encodePacked(zpad,message,uint8(outputLen >> 8),uint8(outputLen & 255), uint8(0),bytes(domain),domainLength);
-        console2.logBytes(b_0);
         bytes32 b_0_hashed = keccak256(b_0);
-        console2.logBytes32(b_0_hashed);
+
         bytes memory b_i = abi.encodePacked(b_0_hashed,uint8(1),bytes(domain),domainLength);
         bytes32 b_i_hashed = keccak256(b_i);
-        console2.logBytes32(b_i_hashed);
         bytes memory newb_i = abi.encodePacked(b_i_hashed ^ b_0_hashed, uint8(2), bytes(domain),domainLength);
         bytes32 newHash =keccak256(newb_i);
         bytes memory newb_ii = abi.encodePacked(newHash ^ b_0_hashed, uint8(3), bytes(domain),domainLength);
         bytes32 secondHash = keccak256(newb_ii);
-        console.logBytes(abi.encodePacked(b_i_hashed,newHash,secondHash));
+
         return abi.encodePacked(b_i_hashed,newHash,secondHash);
     }
     function testHash_to_field(bytes memory rands) public view
     {
+        // Hashing string to two elements of the field should work
         bytes memory expanded = expand_message(rands);
         bytes memory ord = hex"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47";
         bytes memory left = new bytes(48);
@@ -180,18 +115,15 @@ contract test_suiteTest is Test
         BigNumber memory leftBig = BigNumbers.init(left, false);
         BigNumber memory rightBig = BigNumbers.init(right, false);
         BigNumber memory fieldOrder = BigNumbers.init(ord, false);
-        console.logBytes(left);
-        console.logBytes(right);
         BigNumber memory resOne = BigNumbers.mod(leftBig,fieldOrder);
         BigNumber memory resTwo = BigNumbers.mod(rightBig,fieldOrder);
-        console.logBytes(resOne.val);
-        console.logBytes(resTwo.val);
         uint256[2] memory p = bls.hashToField(bytes(domain), rands);
         assert(keccak256(abi.encodePacked(p[0])) == keccak256(resOne.val));
         assert(keccak256(abi.encodePacked(p[1])) == keccak256(resTwo.val));
     }
     function testG1() public noGasMetering
     {
+        // Check that G1=[r]E(Fp) reference points are correctly accepted as signatures
         for(uint256 i = 0; i < 1000; i++)
         {
             uint[] memory iq = g1_signatures[i];
@@ -201,6 +133,7 @@ contract test_suiteTest is Test
     }
     function testG2() public noGasMetering
     {
+        // Check that G2=[r]E'(Fp2) reference points are correctly accepted as pubkeys
         for(uint256 i = 0; i < 1000; i++)
         {
             uint[] memory iq = g2_public_keys[i];
@@ -210,6 +143,8 @@ contract test_suiteTest is Test
     }
     function testE2noG2() public noGasMetering
     {
+        // Check that E'(Fp2) reference points that are not in the r-torsion are correctly rejected from G2
+        // Fails, as it should.
         for(uint256 i = 0; i < 1000; i++)
         {
             uint[] memory iq = e2_non_g2[i];
@@ -219,6 +154,7 @@ contract test_suiteTest is Test
     }
     function testSVDW() public noGasMetering
     {
+        // Verify SVDW implementation
         for(uint256 i = 0; i < 1000; i++)
         {
             uint256[] memory iq = svdw[i];
@@ -230,6 +166,25 @@ contract test_suiteTest is Test
     }
     function testLibraryConsistent() public noGasMetering
     {
+        // Check internal consistency of the library
+
+        // ***WARNING***
+        // current function does not succeed for
+        // "G2_public_keys"[0] =
+        /*{
+            "x":
+            {
+                "c0": "17267733413650230193222922153445270939806798115927372379982088744205711261095",
+                "c1": "15854078293592491502266924041959310439667723715867379089286498124550238459007"
+            },
+            "y":
+            {
+                "c0": "18774499809907314868909550768741913920297412678837442223181886738088894724467",
+                "c1": "1544166576206462055037343427328113268080923218411858629102632928910719259953"
+            }
+        }
+        returns [false, true] from verifySingle()
+        */
         uint256[2] memory pt = bls.hashToPoint(bytes(domain), bytes("Hello World!"));
         assert(bls.isOnCurveG1(pt));
         for(uint256 i = 0; i < 1000; i++)
@@ -249,6 +204,8 @@ contract test_suiteTest is Test
     }
     function testLibConsistentTwo() public noGasMetering
     {
+        // Check internal consistency of the library 2
+        // What's odd is that this function DOES pass
         uint256[2] memory pt = bls.hashToPoint(bytes(domain), bytes("Hello world!"));
         for(uint256 i = 0; i < 1000; i++)
         {
@@ -265,28 +222,4 @@ contract test_suiteTest is Test
             }
         }
     }
-    // The next step is to implement hash_to_field and expand_message from
-    // utils.ts
-
-    // utils.ts is the Tristan-implemented typescript library that
-    // is supposed to precisely mimic the on-chain solidity implementation
-    // BLS.sol
-
-    // WE SHOULD BE ABLE TO USE FUZZER UTILITY TO GENERATE RANDOM BYTES
-    // BUT WE ALSO MIGHT NOT HAVE TO.
-
-    /*/// Returns a random uint256 value.
-    function randomUint() external returns (uint256);
-
-    /// Returns random uin256 value between the provided range (=min..=max).
-    function randomUint(uint256 min, uint256 max) external returns (uint256);*/
-
-    // Verifying a correct implementation there is the first test done.
-
-    // After that, we  switch to verifying the consistency and logic of the
-    // library directly.
-
-    // That just involves feeding our SAGE-generated JSON into the contracts
-    // and correctly getting TRUE or FALSE
-
 }
