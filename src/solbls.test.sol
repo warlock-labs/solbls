495d54f077160b5ad21a9ab9462cdb640d2f5e1e
Mitchell Mikinski <mbm2212@columbia.edu>
2024-07-22 16:12:02 -0500

    formatting changes

--- src/solbls.test.sol
+++ src/solbls.test.sol
@@ -4,70 +4,43 @@ pragma solidity ^0.8;
 import {BLS} from "./BLS.sol";
 
 contract BLSTest {
-    function expandMsgTo96(
-        bytes memory domain,
-        bytes memory message
-    ) external pure returns (bytes memory exp_msg)  {
+    function expandMsgTo96(bytes memory domain, bytes memory message) external pure returns (bytes memory exp_msg) {
         exp_msg = BLS.expandMsgTo96(domain, message);
     }
 
-    function hashToField(
-        bytes memory domain,
-        bytes memory message
-    ) external pure returns (uint256[2] memory p) {
+    function hashToField(bytes memory domain, bytes memory message) external pure returns (uint256[2] memory p) {
         p = BLS.hashToField(domain, message);
     }
 
-    function mapToPoint(
-        uint256 value
-    ) external view returns (uint256[2] memory p) {
+    function mapToPoint(uint256 value) external view returns (uint256[2] memory p) {
         p = BLS.mapToPoint(value);
     }
 
-    function hashToPoint(
-        bytes memory domain,
-        bytes memory message
-    ) external view returns (uint256[2] memory p) {
+    function hashToPoint(bytes memory domain, bytes memory message) external view returns (uint256[2] memory p) {
         p = BLS.hashToPoint(domain, message);
     }
 
-    function verifySingle(
-        uint256[2] memory signature,
-        uint256[4] memory pubkey,
-        uint256[2] memory message
-    )
+    function verifySingle(uint256[2] memory signature, uint256[4] memory pubkey, uint256[2] memory message)
         external
         view
         returns (bool pairingSuccess, bool callSuccess)
     {
-        (pairingSuccess, callSuccess) = BLS.verifySingle(
-            signature,
-            pubkey,
-            message
-        );
+        (pairingSuccess, callSuccess) = BLS.verifySingle(signature, pubkey, message);
     }
 
-    function isOnCurveG1(
-        uint256[2] memory point
-    ) external pure returns (bool _isOnCurve) {
+    function isOnCurveG1(uint256[2] memory point) external pure returns (bool _isOnCurve) {
         _isOnCurve = BLS.isOnCurveG1(point);
     }
 
-    function isOnCurveG2(
-        uint256[4] memory point
-    ) external pure returns (bool _isOnCurve) {
+    function isOnCurveG2(uint256[4] memory point) external pure returns (bool _isOnCurve) {
         _isOnCurve = BLS.isOnCurveG2(point);
     }
 
-    function isValidSignature(
-        uint256[2] memory signature
-    ) external pure returns (bool isValid) {
+    function isValidSignature(uint256[2] memory signature) external pure returns (bool isValid) {
         isValid = BLS.isValidSignature(signature);
     }
 
-    function isValidPublicKey(
-        uint256[4] memory publicKey
-    ) external pure returns (bool isValid) {
+    function isValidPublicKey(uint256[4] memory publicKey) external pure returns (bool isValid) {
         isValid = BLS.isValidPublicKey(publicKey);
     }
 }
